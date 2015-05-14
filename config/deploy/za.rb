2b760440bbb1b5b47b7731c58d90ebc2c5f402fd
Grant Speelman <grant.speelman@ubxd.com>
2015-05-14 19:40:10 +0200

    update deploy and newrelic.yml

--- config/deploy/za.rb
+++ config/deploy/za.rb
@@ -1,3 +1,4 @@
 %w(za_ec za_fs za_gt za_nl za_lp za_mp za_nc za_nw za_wc).each do |za_user|
-  server '41.79.78.65', user: za_user, roles: %w(web app)
+  server "#{za_user.gsub('_','-')}.public-grocery-price-book-api.co.za",
+         user: za_user, roles: %w(web app)
 end
