bf6efef3fea6c089cf696996e8dc08323432d234
Grant Speelman <grant.speelman@ubxd.com>
2015-05-15 08:39:45 +0200

    rubocop:auto_correct

--- config/deploy/za.rb
+++ config/deploy/za.rb
@@ -1,4 +1,4 @@
 %w(za_ec za_fs za_gt za_nl za_lp za_mp za_nc za_nw za_wc).each do |za_user|
-  server "#{za_user.gsub('_','-')}.public-grocery-price-book-api.co.za",
+  server "#{za_user.gsub('_', '-')}.public-grocery-price-book-api.co.za",
          user: za_user, roles: %w(web app)
 end
