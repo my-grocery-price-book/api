e6e5f1dc7e6783f50f8d559665d4f94321940a6b
Grant Speelman <grant.speelman@ubxd.com>
2015-07-05 23:22:26 +0200

    catch enviroment errors and send to rollbar

--- config/enviroment.rb
+++ config/enviroment.rb
@@ -5,23 +5,28 @@ require 'dotenv'
 ENV['RACK_ENV'] ||= 'development'
 Dotenv.load(".env.#{ENV['RACK_ENV']}", '.env')
 
-require 'syslogger'
-LOGGER = Syslogger.new('grocery_api', Syslog::LOG_PID, Syslog::LOG_LOCAL0) unless defined?(LOGGER)
-LOGGER.level = Logger::DEBUG
-LOGGER.info("Loading Api Enviroment for #{`whoami`}")
-
 require 'rollbar'
 Rollbar.configure do |config|
   config.access_token = 'b330dae833714676a4e8c809b11144f6'
   config.disable_monkey_patch = true
   config.environment = ENV['RACK_ENV']
+  config.enabled = (ENV['MUTANT'].nil?)
 end
 
-require 'sequel'
-DB = ENV['MUTANT'] ? Sequel.sqlite : Sequel.connect(ENV['SEQUEL_CONNECT'])
-DB.loggers << LOGGER
+begin
+  require 'syslogger'
+  LOGGER = Syslogger.new('grocery_api', Syslog::LOG_PID, Syslog::LOG_LOCAL0) unless defined?(LOGGER)
+  LOGGER.level = Logger::DEBUG
+  LOGGER.info("Loading Api Enviroment for #{`whoami`}")
 
-Sequel.extension :migration
-Sequel::Migrator.run(DB, './db/migrations')
+  require 'sequel'
+  DB = ENV['MUTANT'] ? Sequel.sqlite : Sequel.connect(ENV['SEQUEL_CONNECT'])
+  DB.loggers << LOGGER
 
-LOGGER.info("Api Enviroment Loaded and DB Migrated for #{`whoami`}")
+  Sequel.extension :migration
+  Sequel::Migrator.run(DB, './db/migrations')
+
+  LOGGER.info("Api Enviroment Loaded and DB Migrated for #{`whoami`}")
+rescue => e
+  Rollbar.critical(e)
+end
