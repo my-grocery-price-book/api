64595d4d646ca9f679a388a62285197f74dd1eba
Grant Speelman <grant.speelman@ubxd.com>
2015-08-07 21:41:54 +0200

    more bootup logging

--- config/enviroment.rb
+++ config/enviroment.rb
@@ -3,9 +3,6 @@ require 'rubygems'
 require 'bundler/setup'
 require 'dotenv'
 ENV['RACK_ENV'] ||= 'development'
-if ENV['RDS_DB_NAME'] && ENV['RDS_USERNAME']
-  ENV['SEQUEL_CONNECT'] = "postgresql://#{ENV['RDS_USERNAME']}:#{ENV['RDS_PASSWORD']}@#{ENV['RDS_HOSTNAME']}:#{ENV['RDS_PORT']}/#{ENV['RDS_DB_NAME']}?pool=5"
-end
 Dotenv.load(".env.#{ENV['RACK_ENV']}", '.env')
 
 require 'rollbar'
@@ -20,14 +17,18 @@ begin
   require 'logger'
   LOGGER = Logger.new("log/#{ENV['RACK_ENV']}.log")
   LOGGER.level = Logger::DEBUG
-  LOGGER.info("Loading Api Enviroment for #{`whoami`}")
+  LOGGER.info("Loading Api Enviroment for user:#{`whoami`} pid:#{Process.pid}")
 
   require 'sequel'
   if ENV['MUTANT']
     DB = Sequel.sqlite
   else
+    LOGGER.info("ORG_SEQUEL_CONNECT: #{ENV['SEQUEL_CONNECT']}")
+    if ENV['RDS_DB_NAME'] && ENV['RDS_USERNAME']
+      ENV['SEQUEL_CONNECT'] = "postgresql://#{ENV['RDS_USERNAME']}:#{ENV['RDS_PASSWORD']}@#{ENV['RDS_HOSTNAME']}:#{ENV['RDS_PORT']}/#{ENV['RDS_DB_NAME']}?pool=5"
+    end
     LOGGER.info("SEQUEL_CONNECT: #{ENV['SEQUEL_CONNECT']}")
-    Sequel.connect(ENV['SEQUEL_CONNECT'], max_connections: 16)
+    DB = Sequel.connect(ENV['SEQUEL_CONNECT'], max_connections: 16)
   end
 
   DB.loggers << LOGGER
